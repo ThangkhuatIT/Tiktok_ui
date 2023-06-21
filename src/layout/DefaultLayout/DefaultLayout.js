8f34764dca57d50d05b8eaf6148ec7446fb44c49
ThangKhuat <khuatvanthang01012001@gmail.com>
2023-06-21 16:29:03 +0700

    newww

--- src/layout/DefaultLayout/DefaultLayout.js
+++ src/layout/DefaultLayout/DefaultLayout.js
@@ -4,7 +4,6 @@ import Sidebar from '../../components/Sidebar';
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss'
 const cx = classNames.bind(styles)
-
 const DefaultLayout = ({ children }) => {
     return (
         <div className={cx('wrapper')}>
